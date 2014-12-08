0e50c991d22b20dbc344d33a205b561bf3b0517e
fiello <azazelnn7@mail.ru>
2014-12-08 22:19:53 +0100

    intermediate catch with lesson to get basics for data loading

--- wp8/SoundBoard/SoundBoard/App.xaml.cs
+++ wp8/SoundBoard/SoundBoard/App.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
+using SoundBoard.DataModel;
 
 // The Pivot Application template is documented at http://go.microsoft.com/fwlink/?LinkID=391641
 
@@ -27,6 +28,20 @@ namespace SoundBoard
     public sealed partial class App : Application
     {
         private TransitionCollection transitions;
+
+        private static SoundModel viewModel = null;
+
+        public static SoundModel ViewModel
+        {
+            get
+            {
+                if (viewModel == null)
+                    viewModel = new SoundModel();
+
+                return viewModel;
+            }
+
+        }
 
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
---
+++ wp8/SoundBoard/SoundBoard/DataModel/SoundModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundBoard.DataModel
+{
+    public class SoundModel
+    {
+        public SoundGroup CustomSounds{ get; set; }
+        public SoundGroup Animals { get; set; }
+        public SoundGroup Cartoons { get; set; }
+        public SoundGroup Taunts { get; set; }
+        public SoundGroup Warnings{ get; set; }
+
+        public bool IsDataLoaded { get; set; }
+
+        public void LoadData()
+        {
+            // 
+            IsDataLoaded = true;
+        }
+    }
+}
