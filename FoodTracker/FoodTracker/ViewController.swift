356b8af98913c11ce6a9b91865eeb0beef38b02f
fiello <azazelnn7@mail.ru>
2015-12-28 23:24:42 +0100

    picture selector

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate
+class ViewController
+    : UIViewController
+    , UITextFieldDelegate
+    , UIImagePickerControllerDelegate
+    , UINavigationControllerDelegate
 {
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +38,29 @@ class ViewController: UIViewController, UITextFieldDelegate
         return true;
     }
     
+    // MARK: UINavigationControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController)
+    {
+        dismissViewControllerAnimated(true, completion: nil);
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage;
+        photoImageView.image = selectedImage;
+        dismissViewControllerAnimated(true, completion: nil);
+    }
+    
     // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer)
+    {
+        nameTextField.resignFirstResponder();
+        let imagePickerController = UIImagePickerController();
+        imagePickerController.sourceType = .PhotoLibrary;
+        imagePickerController.delegate = self;
+        presentViewController(imagePickerController, animated: true, completion: nil)
+        
+    }
     @IBAction func setDefaultLabelText(sender: UIButton)
     {
         mealNameLabel.text = "Default Text";
