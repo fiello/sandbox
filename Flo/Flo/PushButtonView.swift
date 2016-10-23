5fd0677c5e81b784a9d8074b27dcff894bf64779
fiello <azazelnn7@mail.ru>
2016-10-24 00:48:01 +0200

    some basic draw primitives (button & white cross on it)

--- Flo/Flo/PushButtonView.swift
+++ Flo/Flo/PushButtonView.swift
@@ -11,10 +11,51 @@ import UIKit
 @IBDesignable
 class PushButtonView: UIButton {
 
-    override func drawRect(rect: CGRect) {
-        var path = UIBezierPath(ovalInRect: rect)
-        UIColor.blueColor().setFill()
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(ovalIn: rect)
+        UIColor.green.setFill()
         path.fill();
+        
+        //set up the width and height variables
+        //for the horizontal stroke
+        let plusHeight: CGFloat = 5.0
+        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        
+        //create the path
+        let plusPath = UIBezierPath()
+        
+        //set the path's line width to the height of the stroke
+        plusPath.lineWidth = plusHeight
+        
+        //move the initial point of the path
+        //to the start of the horizontal stroke
+        plusPath.move(to: CGPoint(
+            x:bounds.width/2 - plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
+        
+        //add a point to the path at the end of the stroke
+        plusPath.addLine(to: CGPoint(
+            x:bounds.width/2 + plusWidth/2 + 0.5,
+            y:bounds.height/2 + 0.5))
+        
+        //Vertical Line
+        
+        //move to the start of the vertical stroke
+        plusPath.move(to: CGPoint(
+            x:bounds.width/2 + 0.5,
+            y:bounds.height/2 - plusWidth/2 + 0.5))
+        
+        //add the end point to the vertical stroke
+        plusPath.addLine(to: CGPoint(
+            x:bounds.width/2 + 0.5,
+            y:bounds.height/2 + plusWidth/2 + 0.5))
+        
+        //set the stroke color
+        UIColor.white.setStroke()
+        
+        //draw the stroke
+        plusPath.stroke()
+
     }
     
 }
