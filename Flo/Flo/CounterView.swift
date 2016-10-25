34aa4113708638c7b105f242baab763bcb3e70bd
fiello <azazelnn7@mail.ru>
2016-10-25 23:41:46 +0200

    git adding glasses counter

---
+++ Flo/Flo/CounterView.swift
@@ -0,0 +1,88 @@
+//
+//  CounterView.swift
+//  Flo
+//
+//  Created by Gaara on 25/10/16.
+//  Copyright © 2016 Gaara. All rights reserved.
+//
+
+import UIKit
+
+let NumberOfGlasses = 8
+let pi: CGFloat  = CGFloat(M_PI)
+
+@IBDesignable
+class CounterView: UIView {
+
+    @IBInspectable var counter: Int = 5;
+    @IBInspectable var outlineColor: UIColor = UIColor.blue
+    @IBInspectable var counterColor: UIColor = UIColor.orange
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        
+        // 1
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        
+        // 2
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        // 3
+        let arcWidth: CGFloat = 76
+        
+        // 4
+        let startAngle: CGFloat = 3 * pi / 4
+        let endAngle: CGFloat = pi / 4
+ 
+        // 5
+        let path = UIBezierPath(arcCenter: center,
+                                radius: radius/2 - arcWidth/2,
+                                startAngle: startAngle,
+                                endAngle: endAngle,
+                                clockwise: true)
+        
+        // 6
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
+        
+        //Draw the outline
+        
+        //1 - first calculate the difference between the two angles
+        //ensuring it is positive
+        let angleDifference: CGFloat = 2 * pi - startAngle + endAngle
+        
+        //then calculate the arc for each single glass
+        let arcLengthPerGlass = angleDifference / CGFloat(NumberOfGlasses)
+        
+        //then multiply out by the actual glasses drunk
+        let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
+        
+        //2 - draw the outer arc
+        let outlinePath = UIBezierPath(arcCenter: center,
+                                       radius: bounds.width/2 - 2.5,
+                                       startAngle: startAngle,
+                                       endAngle: outlineEndAngle,
+                                       clockwise: true)
+        
+        //3 - draw the inner arc
+        outlinePath.addArc(withCenter: center,
+                                     radius: bounds.width/2 - arcWidth + 2.5,
+                                     startAngle: outlineEndAngle,
+                                     endAngle: startAngle,
+                                     clockwise: false)
+        
+        //4 - close the path
+        outlinePath.close()
+        
+        outlineColor.setStroke()
+        outlinePath.lineWidth = 5.0
+        outlinePath.stroke()
+        
+        
+    }
+ 
+
+}
